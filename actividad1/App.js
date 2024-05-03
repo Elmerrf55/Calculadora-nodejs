4b230b4799e027f6480c900ad5614be7f8c3fea9
Elmer Ivan Reales Flores <20220240@ricaldone.edu.sv>
2024-05-03 12:52:51 -0600

    segunda capeta para ejercicios

--- actividad1/App.js
+++ actividad1/App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
-export default function App() {
+const Calculator = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState(0);
+
+  const handleSum = () => {
+    setResult(parseFloat(num1) + parseFloat(num2));
+  };
+
+  const handleSubtract = () => {
+    setResult(parseFloat(num1) - parseFloat(num2));
+  };
+
+  const handleMultiply = () => {
+    setResult(parseFloat(num1) * parseFloat(num2));
+  };
+
+  const handleDivide = () => {
+    if (parseFloat(num2) !== 0) {
+      setResult(parseFloat(num1) / parseFloat(num2));
+    } else {
+      setResult('Error: Division by zero');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Ingrese un número"
+        value={num1}
+        onChangeText={text => setNum1(text)}
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Ingrese otro número"
+        value={num2}
+        onChangeText={text => setNum2(text)}
+        keyboardType="numeric"
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="+" onPress={handleSum} />
+        <Button title="-" onPress={handleSubtract} />
+        <Button title="*" onPress={handleMultiply} />
+        <Button title="/" onPress={handleDivide} />
+      </View>
+      <Text style={styles.result}>Resultado: {result}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    margin: 100
+    
+  },
+  input: {
+    width: 200,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    margin: 10
+  },
+  buttonContainer: {
+    margin: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginBottom: 50,
+    color: '#ccc'
+    
+   
+  },
+  result: {
+    margin: 10,
+    fontSize: 25
   },
 });
+
+export default Calculator;
